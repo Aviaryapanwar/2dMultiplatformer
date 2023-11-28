6450094d0dfb3cf765c75c9b75ed9d19bf346fa7
Avi <aviaryapanwar@gmail.com>
2023-11-28 23:51:14 +0530

    Fixed player movement

--- js/Player.js
+++ js/Player.js
@@ -1,79 +1,97 @@
-class Player extends Spirit{
-
-
-  constructor({collisionBlocks=[],imageSrc}) {
-    super({imageSrc,frameRate:11})
+class Player extends Spirit {
+  constructor({ collisionBlocks = [], imageSrc }) {
+    super({ imageSrc, frameRate: 11 });
     this.position = {
-      x:200,y:200
-    }
-    this.collisionBlocks = collisionBlocks
- 
-  
- 
+      x: 200,
+      y: 200,
+    };
+    this.collisionBlocks = collisionBlocks;
+
     this.gravityFactor = 0.5;
     this.velocity = {
       x: 0,
       y: 0,
     };
-
-    this.sides = {
-      bottom: this.position.y + this.height,
-    };
   }
 
   update() {
-   
+    
     this.position.x += this.velocity.x;
-    this.hitbox = {
-      position:{x:this.position.x+58,y:this.position.y+34},
-      width:50,height:5
-    }
-    this.checkForHorizontalCollisions()
-    this.applyGravity()
-    this.checkForVerticalCollisions()
+   
+    this.updateHitbox();
+
+    this.checkForHorizontalCollisions();
+    this.applyGravity();
+    this.updateHitbox();
+    this.checkForVerticalCollisions();
+    c.fillRect(
+      this.hitbox.position.x,
+      this.hitbox.position.y,
+      this.hitbox.width,
+      this.hitbox.height
+    );
+    //  c.fillStyle ='blue'
+    //  c.fillRect(this.position.x,this.position.y,this.width,this.height);
     this.draw();
   }
+  updateHitbox() {
+    this.hitbox = {
+      position: { x: this.position.x + 58, y: this.position.y + 34 },
+      width: 50,
+      height: 53,
+    };
+  }
+  checkForHorizontalCollisions() {
+    for (let i = 0; i < this.collisionBlocks.length; i++) {
+      const block = this.collisionBlocks[i];
 
-  checkForHorizontalCollisions(){
-    for(let i = 0; i< this.collisionBlocks.length;i++){
-      const block = this.collisionBlocks[i] 
-  
-      if(block.position.x + block.width >= this.position.x &&
-        this.position.x +this.width >= block.position.x &&
-        this.position.y + this.height >= block.position.y &&
-        block.position.y + block.height >= this.position.y)
-        {
-          if(this.velocity.x  < -1){this.position.x = block.position.x + block.width + 0.01
-          break}
-          if(this.velocity.x  >  1){this.position.x = block.position.x -this.width - 0.01
-          break}
-   
+      if (
+        block.position.x + block.width >= this.hitbox.position.x &&
+        this.hitbox.position.x + this.hitbox.width >= block.position.x &&
+        this.hitbox.position.y + this.hitbox.height >= block.position.y &&
+        block.position.y + block.height >= this.hitbox.position.y
+      ) {
+        if (this.velocity.x < -1) {
+          const offset = this.hitbox.position.x - this.position.x
+          this.position.x = block.position.x + block.width -offset+ 0.01;
+          break;
         }
+        if (this.velocity.x > 1) {
+          const offset =
+            this.hitbox.position.x - this.position.x + this.hitbox.width;
+          this.position.x = block.position.x -offset - 0.01;
+          break;
+        }
+      }
     }
   }
-  checkForVerticalCollisions(){
-    
-    for(let i = 0; i< this.collisionBlocks.length;i++){
-      const block = this.collisionBlocks[i] 
-  
-      if(block.position.x + block.width >= this.position.x &&
-        this.position.x +this.width >= block.position.x &&
-        this.position.y + this.height >= block.position.y &&
-        block.position.y + block.height >= this.position.y)
-        {
-          if(this.velocity.y  < 0){this.position.y = block.position.y + block.height + 0.01
-          this.velocity.y = 0
-          break
+  checkForVerticalCollisions() {
+    for (let i = 0; i < this.collisionBlocks.length; i++) {
+      const block = this.collisionBlocks[i];
+
+      if (
+        block.position.x + block.width >= this.hitbox.position.x &&
+        this.hitbox.position.x + this.hitbox.width >= block.position.x &&
+        this.hitbox.position.y + this.hitbox.height >= block.position.y &&
+        block.position.y + block.height >= this.hitbox.position.y
+      ) {
+        if (this.velocity.y < 0) {
+          this.velocity.y = 0;
+          const offset = this.hitbox.position.y - this.position.y;
+          this.position.y = block.position.y + block.height - offset + 0.01;
+          break;
         }
-          if(this.velocity.y  >  0){this.velocity.y = 0
-            this.position.y = block.position.y -this.height - 0.01
-          break
-          }
-   
+        if (this.velocity.y > 0) {
+          this.velocity.y = 0;
+          const offset =
+            this.hitbox.position.y - this.position.y + this.hitbox.height;
+          this.position.y = block.position.y - offset - 0.01;
+          break;
         }
+      }
     }
   }
-  applyGravity(){
+  applyGravity() {
     this.velocity.y += this.gravityFactor;
     this.position.y += this.velocity.y;
   }
